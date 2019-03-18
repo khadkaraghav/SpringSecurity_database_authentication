918360248db957fccbb6f74ce8da7319fe7de792
Raghav Khadka <khadkaraghav@gmail.com>
2019-03-18 17:05:08 -0400

    Update setUser

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -72,7 +72,7 @@ public class User {
     public void setPassword(String password) {
         BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
         this.password=passwordEncoder.encode(password);
-        this.password = password;
+        
     }
 
     public String getFirstName() {
